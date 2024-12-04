a11e90d0d63b8a88b359bb248f3df38caaf12694
MichelineWL <michelinewijaya@gmail.com>
2024-12-04 15:57:30 +0700

    fix: conflict

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,15 @@
 import FlutterMacOS
 import Foundation
 
+<<<<<<< Updated upstream
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+=======
+import path_provider_foundation
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+>>>>>>> Stashed changes
 }
